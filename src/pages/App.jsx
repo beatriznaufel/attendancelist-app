562adae6dd00a0b2ed49a3775ec96f594d79f2fc
beatriznaufel <mariabncarvalho@gmail.com>
2022-07-13 15:24:45 -0300

    Starting application

---
+++ src/pages/App.jsx
@@ -0,0 +1,11 @@
+import './styles.css'
+
+export default function App() {
+  return (
+    <div className="container">
+    <h1>Lista de presença</h1>
+    <input type="text" placeholder="Digite o nome..."/>
+    <button type="button">Adicionar</button>
+    </div>
+  )
+}
