2f30eaf6c1148c16b15162150da825471305f11f
beatriznaufel <mariabncarvalho@gmail.com>
2023-07-13 21:56:55 -0300

    style: change layout

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,44 +1,48 @@
-import React, { useState } from 'react';
-import './styles.css';
-import { Card } from '../components/card';
+import React, { useState } from "react";
+import "./styles.css";
+import { Card } from "../components/card";
 
 export default function App() {
- const [studentName, setStudentName] = useState();
- const [students, setStudents] = useState ([]);
+  const [studentName, setStudentName] = useState();
+  const [students, setStudents] = useState([]);
 
- function handleAddStudent(){
-  const newStudent = {
-    name: studentName,
-    time: new Date().toLocaleTimeString("pt-br", {
-    hour: '2-digit',
-minute: '2-digit',
-second: '2-digit',
-    })
-  };
+  function handleAddStudent() {
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString("pt-br", {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+      }),
+    };
 
-  setStudents(prevState => [...prevState, newStudent]);
- }
+    setStudents((prevState) => [...prevState, newStudent]);
+  }
 
   return (
     <div className="container">
       <header>
-    <h1>Attendance List</h1>
-    <div>
-      <strong>Maria Beatriz</strong>
-      <img src="https://github.com/beatriznaufel.png" alt="Profile photo" />
-    </div>
-    </header>
+        <h1>Attendance List</h1>
+        <div>
+          <strong>Maria Beatriz</strong>
+          <img src="https://github.com/beatriznaufel.png" alt="Profile photo" />
+        </div>
+      </header>
 
-    <input type="text" 
-    placeholder="Type your name"
-    onChange={e => setStudentName(e.target.value)}
-    />
-    <button type="button" onClick={handleAddStudent}>Add</button>
+      <div className="input-button">
+        <input
+          type="text"
+          placeholder="Type your name"
+          onChange={(e) => setStudentName(e.target.value)}
+        />
+        <button type="button" onClick={handleAddStudent}>
+          Add
+        </button>
+      </div>
 
-{
-  students.map(student => 
-  <Card key={student.time} name={student.name} time={student.time}/>)
-}
+      {students.map((student) => (
+        <Card key={student.time} name={student.name} time={student.time} />
+      ))}
     </div>
-  )
+  );
 }
